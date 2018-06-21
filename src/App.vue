ef296a77244c829dffa22e0f409ae6680089a48b
Justの磊哥 <legold520@163.com>
2018-06-21 17:24:50 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="app">    
-      <keep-alive>
+  <div id="app">   
+
+    <transition :name="transitionName">
+      <keep-alive class="app_view">
         <router-view/>
       </keep-alive>
+    </transition> 
     
     <!-- <div>{{ loginMsg }}</div> -->
   </div>
@@ -14,7 +17,22 @@ export default {
   name: 'App',
   data() {
     return {
-       loginMsg: '今日头条'
+       loginMsg: '今日头条',
+       transitionName: 'slide-left',
+       timer: null, 
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+       let that = this;
+       console.log(to, from);
+       //如果to索引大于from索引,判断为前进状态,反之则为后退状态
+       if(to.meta.index > from.meta.index){
+          // 设置动画明后才能
+          this.transitionName = 'slide-left';
+       }else{
+          this.transitionName = 'slide-right';
+       }
     }
   },
   methods: {
@@ -61,6 +79,36 @@ body{
   .fade{
      -webkit-transition-duration: .2s;
      transition-duration: .2s;
+  }
+
+  .app_view{
+    opacity: 1;
+  }
+
+  .slide-right-enter-active,
+  .slide-right-leave-active,
+  .slide-left-enter-active,
+  .slide-left-leave-active{
+    will-change: transform;
+    -webkit-transition: all 300ms cubic-bezier(.19,.53,.21,1.01);
+    transition: all 300ms cubic-bezier(.19,.53,.21,1.01);
+  }
+
+  .slide-right-enter {
+    opacity: 0.05; 
+    transform: translate3d(-60%, 0, 0);
+  }
+  .slide-right-leave-active {
+    opacity: 0.05; 
+    transform: translate3d(60%, 0, 0);
+  }
+  .slide-left-enter {
+    opacity: 0.05; 
+    transform: translate3d(60%, 0, 0);
+  }
+  .slide-left-leave-active {
+    opacity: 0.05;
+    transform: translate3d(-60%, 0, 0);
   }
 }
 </style>
